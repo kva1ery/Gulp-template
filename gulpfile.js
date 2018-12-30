ba53b61a6bac0db9971cac91fcf9fab8f6744977
Valeriy Kokorev <kokorev.valery@gmail.com>
2018-12-30 12:05:31 +0700

    Добавил autoprefixer, sourcemap и уведомление об ошибках для sass

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,14 @@ let gulp = require('gulp'),
 
 gulp.task('scss', function () {
     return gulp.src('src/scss/**/*.scss')
+        .pipe(gp.sourcemaps.init())
         .pipe(gp.sass({outputStyle: 'expanded'}))
+        .pipe(gp.autoprefixer(['last 5 versions', '> 1%'], { cascade: true }))
+        .on("error", gp.notify.onError({
+            message: "Error: <%= error.message %>",
+            title: "scss error"
+        }))
         .pipe(gp.csso())
+        .pipe(gp.sourcemaps.write())
         .pipe(gulp.dest('build/css'));
 });
