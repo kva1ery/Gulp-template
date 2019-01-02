74bbd1e7eebb128520264e867a7651a3c4180698
Valeriy Kokorev <kokorev.valery@gmail.com>
2019-01-02 18:39:41 +0700

    Добавил плагин BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 'use strict';
 
 let gulp = require('gulp'),
-    gp   = require('gulp-load-plugins')();
+    gp   = require('gulp-load-plugins')(),
+    browserSync = require('browser-sync').create();
+
+gulp.task('html', function () {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build'))
+        .on('end', browserSync.reload);
+});
 
 gulp.task('scss', function () {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src('src/scss/*.scss')
         .pipe(gp.sourcemaps.init())
         .pipe(gp.sass({outputStyle: 'expanded'}))
         .pipe(gp.autoprefixer(['last 5 versions', '> 1%'], { cascade: true }))
@@ -14,15 +21,27 @@ gulp.task('scss', function () {
         }))
         .pipe(gp.csso())
         .pipe(gp.sourcemaps.write())
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('build/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('watch', function () {
-   gulp.watch('src/scss/**/*.scss', gulp.series('scss'));
+    gulp.watch('src/*.html', gulp.series('html'));
+    gulp.watch('src/scss/**/*.scss', gulp.series('scss'));
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
 });
 
 gulp.task('default',
     gulp.series(
-        gulp.parallel('scss'),
-        'watch'
+        gulp.parallel('html', 'scss'),
+        gulp.parallel('watch', 'serve')
     ));
